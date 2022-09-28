5e83413d7869cbf2e24e16a48b22bdfb0ec94f26
ershiyi21 <68776500+ershiyi21@users.noreply.github.com>
2022-09-29 05:10:04 +0800

    Update 21media.sh

--- nastools/21media.sh
+++ nastools/21media.sh
@@ -241,7 +241,7 @@ response = requests.post('${emby_url}/emby/Library/Refresh', params=params, head
 
 #安装qbittorrent
  echo "1.安装docker版qbittorrent【默认情况】"
- echo "2.安装宿主机版qbittorrent【仅适用于debian&ubuntu x86系统，可能存在奇怪问题】"
+ echo "2.安装宿主机版qbittorrent【仅适用于debian&ubuntu x86系统，不支持ARM，而且可能存在奇怪问题】"
  echo "3.不安装qbittorrent"
  read -r -p "请输入: " qbittorrentinstall
  case $qbittorrentinstall in
