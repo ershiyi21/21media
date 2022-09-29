153e45f1cfe473f621abecd5cfee336cbfda8a23
ershiyi21 <68776500+ershiyi21@users.noreply.github.com>
2022-09-29 22:40:14 +0800

    Update 21media.sh

--- nastools/21media.sh
+++ nastools/21media.sh
@@ -369,7 +369,7 @@ cat /home/nastools/config/logs/run.txt
 21update() {
 [[ -f /root/21media.sh ]] && rm /root/21media.sh
 wget -P /root https://raw.githubusercontent.com/ershiyi21/media21/main/nastools/21media.sh
-21shortcut
+mv /root/21media.sh /home/shh/21media.sh
 echo "更新完毕,即将退出脚本.可执行[21media]重新打开脚本."
 exit
 }
