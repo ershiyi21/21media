552f9b037c8c6ec679431b93378becb31e795054
ershiyi21 <68776500+ershiyi21@users.noreply.github.com>
2022-11-11 14:43:11 +0800

    Update dnschange.sh

--- script/dnschange.sh
+++ script/dnschange.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-function dnschang() {
+[[ $EUID -ne 0 ]] && echo -e "必须使用root用户运行此脚本！" && exit 1
+[[ ! -f /etc/resolv.conf ]] && echo "该脚本修改DNS方法 不适合本系统！"
+[[ ! -f /etc/resolv.conf ]] && exit 1
+
+function dnsset() {
     [[ -z "$1" ]] && dns1=$1 || read -r -p "请输入DNS IP: " dns1
-    cp /etc/resolv.conf /etc/resolv.conf.back
+    mv -f /etc/resolv.conf /etc/resolv.conf.dnsback
     echo "nameserver ${dns1}" > /etc/resolv.conf
     echo "nameserver 8.8.8.8" >> /etc/resolv.conf
     sudo chattr +i /etc/resolv.conf
@@ -13,9 +17,8 @@ function dnschang() {
     
     else
     echo "DNS设置失败，恢复原来系统设置"
-    sudo chattr -i /etc/resolv.conf
     rm /etc/resolv.conf
-    mv /etc/resolv.conf.back /etc/resolv.conf 
+    mv -f /etc/resolv.conf.dnsback /etc/resolv.conf 
     
     fi
 }
@@ -23,9 +26,7 @@ function dnschang() {
 function dnsback() {
     [[ ! -f /etc/resolv.conf.back ]] || echo "无系统dns备份,退出脚本..." 
     [[ ! -f /etc/resolv.conf.back ]] || exit 1
-    sudo chattr -i /etc/resolv.conf
-    rm /etc/resolv.conf
-    mv /etc/resolv.conf.back /etc/resolv.conf
+    mv -f /etc/resolv.conf.dnsback /etc/resolv.conf
     echo "系统dns已恢复,如还未恢复，请手动重启：reboot"
 }
 
@@ -38,7 +39,7 @@ echo "3.退出脚本"
 read -r -p selectnum
 case $selectnum in
     1)
-    dnschang
+    dnsset
     ;;
     2)
     dnsback
@@ -52,7 +53,7 @@ esac
 
 case $1 in
     [1sS])
-	  dnschang $2
+	  dnsset $2
 	  ;;
     [2bB])
 	  dnsback
