d4ad217ca3726d68f16595f956d5bccb22905eb2
ershiyi21 <68776500+ershiyi21@users.noreply.github.com>
2022-11-11 14:29:26 +0800

    Update dnschange.sh

--- script/dnschange.sh
+++ script/dnschange.sh
@@ -1,49 +1,52 @@
 #!/bin/sh
 
 function dnschang() {
-[[ -z "$1" ]] && dns1=$1 || read -r -p "请输入DNS IP: " dns1
-cp /etc/resolv.conf /etc/resolv.conf.back
-echo "nameserver ${dns1}" > /etc/resolv.conf
-echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-sudo chattr +i /etc/resolv.conf
-dns2=`nslookup bing.com | grep Server | awk '{print $2}'`
+    [[ -z "$1" ]] && dns1=$1 || read -r -p "请输入DNS IP: " dns1
+    cp /etc/resolv.conf /etc/resolv.conf.back
+    echo "nameserver ${dns1}" > /etc/resolv.conf
+    echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+    sudo chattr +i /etc/resolv.conf
+    dns2=`nslookup bing.com | grep Server | awk '{print $2}'`
 
-if [[ ${dns1} == "${dns2}" ]] ,then
-    echo "系统DNS已永久设置为 ${dns1} "
-else
+    if [[ ${dns1} == "${dns2}" ]] ,then
+        echo "系统DNS已永久设置为 ${dns1} "
+    
+    else
     echo "DNS设置失败，恢复原来系统设置"
-	sudo chattr -i /etc/resolv.conf
-	rm /etc/resolv.conf
-	mv /etc/resolv.conf.back /etc/resolv.conf 
-fi
+    sudo chattr -i /etc/resolv.conf
+    rm /etc/resolv.conf
+    mv /etc/resolv.conf.back /etc/resolv.conf 
+    
+    fi
 }
 
 function dnsback() {
     [[ ! -f /etc/resolv.conf.back ]] || echo "无系统dns备份,退出脚本..." 
-	[[ ! -f /etc/resolv.conf.back ]] || exit 1
-	sudo chattr -i /etc/resolv.conf
-	rm /etc/resolv.conf
-	mv /etc/resolv.conf.back /etc/resolv.conf
-	echo "系统dns已恢复,如还未恢复，请手动重启：reboot"
+    [[ ! -f /etc/resolv.conf.back ]] || exit 1
+    sudo chattr -i /etc/resolv.conf
+    rm /etc/resolv.conf
+    mv /etc/resolv.conf.back /etc/resolv.conf
+    echo "系统dns已恢复,如还未恢复，请手动重启：reboot"
 }
 
 
-menu() {
+function menu() {
 echo "1.设置DNS"
 echo "2.恢复系原来DNS"
 echo "3.退出脚本"
+
 read -r -p selectnum
 case $selectnum in
     1)
-	  dnschang
-	  ;;
-	2)
-	  dnsback
-	  ;;
-	*)
-	  echo "输入错误"
-	  menu
-	  ;;
+    dnschang
+    ;;
+    2)
+    dnsback
+    ;;
+    *)
+    echo "输入错误"
+    menu
+    ;;
 esac
 }
 
@@ -51,10 +54,10 @@ case $1 in
     [1sS])
 	  dnschang $2
 	  ;;
-	[2bB])
+    [2bB])
 	  dnsback
 	  ;;
-	*)
+        *)
 	  menu
 	  ;;
 esac
