063bff5ab8f2ae5e3c22cc616c6651f6a1ba0649
ershiyi21 <68776500+ershiyi21@users.noreply.github.com>
2022-11-11 15:30:40 +0800

    Update dnschange.sh

--- script/dnschange.sh
+++ script/dnschange.sh
@@ -3,8 +3,12 @@
 [[ $EUID -ne 0 ]] && echo -e "必须使用root用户运行此脚本！" && exit 1
 [[ ! -f /etc/resolv.conf ]] && echo "该脚本修改DNS方法 不适合本系统！"
 [[ ! -f /etc/resolv.conf ]] && exit 1
-sudo apt update || sudo yum update
-sudo apt install dnsutils -y || sudo yum install bind-utils -y
+
+nslookup bing.com >/dev/null 2>$1
+if [[ $? =! 0 ]];then
+    sudo apt update || sudo yum update
+    sudo apt install dnsutils -y || sudo yum install bind-utils -y
+fi
 
 function dnsset() {
     [[ -z "$1" ]] && dns1=$1 || read -r -p "请输入DNS IP: " dns1
@@ -14,7 +18,7 @@ function dnsset() {
     sudo chattr +i /etc/resolv.conf
     dns2=`nslookup bing.com | grep Server | awk '{print $2}'`
 
-    if [[ ${dns1} == "${dns2}" ]] ;then
+    if [[ ${dns1} == "${dns2}" ]] ,then
         echo "系统DNS已永久设置为 ${dns1} "
     
     else
@@ -38,13 +42,16 @@ echo "1.设置DNS"
 echo "2.恢复系原来DNS"
 echo "3.退出脚本"
 
-read -r -p "请输入数字： " selectnum
+read -r -p "请输入数字：" selectnum 
 case $selectnum in
     1)
     dnsset
     ;;
     2)
     dnsback
+    ;;
+    3)
+    exit 1
     ;;
     *)
     echo "输入错误"
