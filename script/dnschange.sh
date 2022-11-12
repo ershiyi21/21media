71206ca313e4e7231bb96583bc0d409dd99cbf9e
ershiyi21 <68776500+ershiyi21@users.noreply.github.com>
2022-11-12 15:36:48 +0800

    Update dnschange.sh

--- script/dnschange.sh
+++ script/dnschange.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-[[ $EUID -ne 0 ]] && echo -e "必须使用root用户运行此脚本！\n" && exit 1
+##环境检测
+[[ $EUID -ne 0 ]] && echo -e "必须使用root用户运行此脚本！\n" && exit 2
 touch /etc/resolv.conf.test && chattr +i /etc/resolv.conf.test >/dev/null 2>&1
-[[ $? != 0 ]] && echo -e "缺少chattr，该脚本修改DNS方法 不适合本系统！\n" && exit 2
+[[ $? != 0 ]] && echo -e "缺少chattr，该脚本修改DNS方法 不适合本系统！\n" && exit 3
 chattr -i /etc/resolv.conf.test >/dev/null 2>&1 && rm /etc/resolv.conf.test
 
+##DNS设置
 function dnsset() {
     [[ -n "$1" ]] && dns1=$1 || read -r -p "请输入DNS IP: " dns1
     
@@ -13,59 +15,71 @@ function dnsset() {
     rm -f /etc/resolv.conf >/dev/null 2>&1
     domain=`cat /etc/resolv.conf.dnsback | grep domain`
     search=`cat /etc/resolv.conf.dnsback | grep search`
+    options=`cat /etc/resolv.conf.dnsback | grep options`
     sortlist=`cat /etc/resolv.conf.dnsback | grep sortlist`
     
     echo "${domain}" > /etc/resolv.conf
     echo "${search}" >> /etc/resolv.conf
+    echo "${options}" >> /etc/resolv.conf
     echo "${sortlist}" >> /etc/resolv.conf   
     echo "nameserver ${dns1}" >> /etc/resolv.conf
     echo "nameserver 1.1.1.1" >> /etc/resolv.conf
   
     chattr +i /etc/resolv.conf
-    echo -e "系统DNS已设置为 ${dns1} ,即将联网检测是否设置成功...\n"
+    echo -e "系统DNS已设置为 ${dns1} ,即将检测是否设置成功...\n"
 }
 
-function dnscheck() {
+##DNS获取,优先输出联网检测结果
+function dnsget() {
+    local_dns=`cat /etc/resolv.conf | grep nameserver | head -n 1 | awk '{print $2}'`
+    dnsget=${local_dns}
     
+    nslookup bing.com >/dev/null 2>&1
+    if [[ $? != 0 ]] ;then
+        sudo apt update || sudo yum update
+        sudo apt install dnsutils -y || sudo yum install bind-utils -y
+        nslookup bing.com >/dev/null 2>&1
+        [[ $? != 0 ]] && return 1
+    fi
+    
+    online_dns=`nslookup bing.com | grep Server | awk '{print $2}'`
+    dnsget=${online_dns}
+    return 0
+}
+
+##DNS是否设置成功检测
+function dnscheck() {
     dns1=$1
-    dnsquery
-    dns2=${dns}
+    dnsget
+    [[ $? == 0 ]] && dns_type="联网"
+    [[ $? == 1 ]] && dns_type="本地"
+    dns2=${dnsget}
     
     dns3=`ip_type ${dns1}`
     dns4=`ip_type ${dns2}`
     
     if [[ ${dns3} == "${dns4}" ]] ;then
-        echo -e "脚本检测确认，系统DNS已永久锁定为 ${dns1} \n"
-        return 0
-    else        
-        chattr -i /etc/resolv.conf
+	echo -e "${dns_type}检测显示，系统DNS已永久锁定为 ${dns1} \n"
+	return 0
+    else
+	chattr -i /etc/resolv.conf
         mv -f /etc/resolv.conf.dnsback /etc/resolv.conf 
-	echo -e "脚本检测确定，DNS设置失败，DNS设置已自动恢复为原来系统的DNS设置\n"
-        return 4
+	echo -e "${dns_type}检测显示，DNS设置失败；DNS设置已自动恢复为原来系统的DNS设置\n"
+	return 1
     fi
 }
 
+##DNS恢复系统设置
 function dnsback() {
     [[ ! -f /etc/resolv.conf.dnsback ]] && echo -e "无系统dns备份,退出脚本...\n" 
     [[ ! -f /etc/resolv.conf.dnsback ]] && return 2
     chattr -i /etc/resolv.conf && \
     mv -f /etc/resolv.conf.dnsback /etc/resolv.conf && \
     echo -e "DNS设置已恢复为原来系统的DNS设置\n" && \
-    return 0 || return 5
+    return 0 || return 1
 }
 
-function dnsquery() {
-    nslookup bing.com >/dev/null 2>&1
-    if [[ $? != 0 ]] ;then
-        sudo apt update || sudo yum update
-        sudo apt install dnsutils -y || sudo yum install bind-utils -y
-        nslookup bing.com >/dev/null 2>&1
-        [[ $? != 0 ]] && echo "nslookup相关包安装失败！退出DNS设置检测" && exit 3
-    fi
-    
-    dns=`nslookup bing.com | grep Server | awk '{print $2}'`
-}
-
+##IPV6地址格式转换
 function ip_type() {
     if [[ -n `echo $1 | grep ":"` ]] ;then
         ipv6calc -v >/dev/null 2>&1
@@ -73,17 +87,18 @@ function ip_type() {
             sudo apt update || sudo yum update
             sudo apt install ipv6calc -y || sudo yum install ipv6calc -y
             ipv6calc -v >/dev/null 2>&1
-            [[ $? != 0 ]] && "ipv6calc安装失败!退出DNS设置检测" && exit 6
+            [[ $? != 0 ]] && "ipv6calc安装失败!退出DNS设置检测" && exit 4
         fi
 	
 	iprt=`ipv6calc --addr2compaddr -q $1`
-        [[ $? != 0 ]] && echo "ipv6输入有误，请检查！退出DNS设置检测" && exit 7
+        [[ $? != 0 ]] && echo "ipv6输入有误，请检查！退出DNS设置检测" && exit 5
         echo ${iprt}
     else
         echo $1
     fi
 }
 
+##可视化Shell界面
 function menu() {
     echo
     echo "0.退出脚本"
@@ -109,8 +124,8 @@ function menu() {
           exit $?
           ;;
 	3)
-          dnsquery
-	  echo -e "系统现在使用的首选DNS服务器为：${dns}\n"
+          dnsget
+	  echo -e "系统当前首选DNS服务器为：${dnsget}\n"
 	  ;;
         *)
           echo -e "\n输入错误，请重新输入！\n"
@@ -119,20 +134,23 @@ function menu() {
     esac
 }
 
+##带参一键运行
 case $1 in
     [1sS])
           echo
 	  dnsset $2
 	  dnscheck ${dns1}
+	  exit $?
 	  ;;
     [2bB])
 	  echo
 	  dnsback
+	  exit $?
 	  ;;
     [3qQ])
           echo
-	  dnsquery
-	  echo -e "系统现在使用的首选DNS服务器为：${dns}\n"
+	  dnsget
+	  echo -e "系统当前首选DNS服务器为：${dnsget}\n"
 	  ;;
 	 *)
 	  menu
